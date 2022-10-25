80405e1aac7caa33c8d89d7a6358e2f627771bbd
santyarboleda <sarboledaq@unal.edu.co>
2022-10-24 23:05:06 -0500

    fixing structure

---
+++ docs/source/conf.py
@@ -0,0 +1,31 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "Python, Big data, Machine Learning y Cloud Computing"
+copyright = "2022, Santiago Arboleda"
+author = "Santiago Arboleda"
+release = "0.1"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["myst_parser", "sphinx.ext.duration", "nbsphinx"]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+nbsphinx_allow_errors = True
+language = "es"
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+import sphinx_rtd_theme, furo
+
+html_theme = "sphinx_rtd_theme"
+# html_theme = "classic"
+html_static_path = ["_static"]
